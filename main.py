9f23bdc354447dec6bc57fe596fc7b91e7b464e3
adok <tima02021996@gmail.com>
2022-08-09 16:20:02 +0600

    big changes

--- main.py
+++ main.py
@@ -1,52 +1,95 @@
-from dis import Instruction
+from array import array
+from asyncio import tasks
+from cgitb import text
+from queue import Empty
+import string
+from unicodedata import category
+from webbrowser import get
 from bs4 import BeautifulSoup
+import asyncio
+import aiohttp
+import http 
 import requests
 
-# url = "https://mirprivoda.ru/katalog/zubchatye-shesterni/"
+baseUrl = 'https://rusdorznak.ru'
+uslugiUrl = 'https://rusdorznak.ru/uslugi'
+# cat_dict = {}   
+
+category_urls= []
+def main():
+    req = requests.get(uslugiUrl)
+    
+    
+    soup = BeautifulSoup(req.text, "lxml")
+    catalog_items = soup.find_all("a", class_ = "production__item")
+    for catalog_item in catalog_items:
+        name = catalog_item.text.strip()
+        url = baseUrl + catalog_item.attrs["href"].strip()
+        # print(name)
+        # print(baseUrl + catalog_item.attrs["href"].strip())
+        # category_urls.append(url)
+        # ParsePage(baseUrl + catalog_item.attrs["href"].strip())
+        # print(baseUrl + catalog_item.attrs["href"].strip())
+        ParseCards(baseUrl + catalog_item.attrs["href"].strip())
+        # for category_url in category_urls:
+
+      
+            
 
 
-# req = requests.get(url)
-# src = req.text
-# with open("index1.html", "w") as file:
-#     file.write(src)
+# sub_cat_dict = []
+def ParsePage(url):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "lxml")
+    page_catalog = soup.find_all("a", class_ = "production__item")
+    for catalog_item in (page_catalog):
+        sub_cat_name = catalog_item.text.strip()
+        sub_cat_url = url + catalog_item.attrs["href"].strip()
+        # sub_cat_dict[sub_cat_name] = sub_cat_url
+        # print(sub_cat_name)
+        # print(url + catalog_item.attrs["href"].strip())
+        ParseSigns(baseUrl + catalog_item.attrs["href"].strip())
 
-with open("index1.html") as file:
-    src = file.read()
+# sign_dict= {}
+def ParseSigns(url):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "lxml")
+    signs_catalog = soup.find_all("a", class_ = "card-block__top")
+    for signs_name in signs_catalog:
+        sign_name = signs_name.text.strip()
+        sign_href = baseUrl + signs_name.attrs["href"].strip()
+        # print(baseUrl + signs_name.attrs["href"].strip())
+        GetSignInfo(sign_href)
+        # print(baseUrl + sign_name.attrs["href"].strip())
+        # sign_url = sign_name.
+        # print(signs_name.text.strip())
 
+def ParseCards(url):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "lxml")
+    cards_catalog = soup.find('div', class_='wrapper').find_all("form", class_= "product-list__card")
+    for card_names in cards_catalog:
+        name = card_names.find('a', class_='product-list__description-title').text.strip()
+        # card_url = baseUrl + card_names.attrs['href'].strip()
+        print(card_names.attrs['href'].strip())
+        # GetSignInfo(card_url)
+        # print('---------------')
+        # count += 1
+        # print(name)
 
-soup = BeautifulSoup(src, "lxml")
-# count = 0
-# def main():
-#     get_names("active")
+def GetSignInfo(url):
+    # print(url)
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "lxml")
+    sign_info = soup.find('ul', class_= 'c-dropdown__list')
+    if sign_info == None:
+        pass  
+    else:
+        attributes = sign_info.find_all('li', class_ = 'c-dropdown__item')
+        for attribute_name in attributes:
+            print(attribute_name.text)  
 
-# instructions= soup.find("span", itemprop="name")
-# try:
-#     method = str.replace(instructions.get_text(strip=True),". ",".")
-#     method = str.replace(method, ". ", ".")
-#     method = (str.replace(method, ".",".\n"))
-# except AttributeError:
-#     print(instructions)
+if __name__ == "__main__":
+    main()
 
-# all_products=soup.find_all(class_ ="active")
-# for item in all_products:
-#     print(item.text)
-# def get_names(a):
-all_targets = soup.find(class_= "profuct_top_line").find("a").find_all("th")
-for names in all_targets:
-    print(names.text)
-#         name = soup.find(class_ = "")
-    # print(names.href)
-    # for name in names:
-    #     all_names = soup.find_all(class_ = "name")
-    #     print(all_names.text)
-# for item in all_names:
-#     item_text = item.text
-#     print(f"{item_text}")
-    # for ref in item:
-    #     href = ref.get("href")
-    # print(item.text)
-    # count = count +1
-    # print(item)
-    # item_href = item.get("href")
-    # print(item.text)
-
+# print(category_urls)
